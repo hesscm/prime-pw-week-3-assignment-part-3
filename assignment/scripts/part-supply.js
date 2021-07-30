dfd0f03b7a8f7ceea28438e1bcc9126ef28b31b0
Chris Hess <chrishessmusic@gmail.com>
2021-07-30 14:56:42 -0500

    completed week 3 assignment part 3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -54,11 +54,52 @@ for (let i = 0; i < supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
+for (let i of supplyChanges) {
+  /*for of already knows it is iterating through supplyChanges, so we
+    do not need to write supplyChanges[i]*/
+  console.log('Value:', i);
+  if (i > 0) {
+    console.log(`Added ${i} parts.`);
+  }
+  else if (i === 0) {
+    console.log('No Change.');
+  }
+  /*This could indicate if the value is negative but it is not necessary
+    as it is the only other possibility*/
+  else {
+    console.log(`Removed ${i} parts.`);
+  }
 
+}
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
-
+//initialize a variable outside of the loop
+let y = 0;
+while (y < supplyChanges.length) {
+  console.log('Value:', supplyChanges[y]);
+  if (supplyChanges[y] > 0) {
+    console.log(`Added ${supplyChanges[y]} parts.`);
+  }
+  else if (supplyChanges[y] === 0) {
+    console.log('No Change.');
+  }
+  /*This could indicate if the value is negative but it is not necessary
+    as it is the only other possibility*/
+  else {
+    console.log(`Removed ${supplyChanges[y]} parts.`);
+  }
+  y++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('9. Total supplies available is:');
+//Initialize a variable to hold total parts
+let totalParts = 0;
+//Loop to iterate through the array
+for (var i = 0; i < supplyChanges.length; i++) {
+  //Update totalParts by adding each element of supplyChanges
+  totalParts = supplyChanges[i] + totalParts;
+}
+//Log is outside of the loop so you only have one total answer
+console.log('Total supplies:', totalParts);
